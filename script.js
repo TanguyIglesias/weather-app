c996fd0bee6f29dc190a417b337a2c235d079ac6
TanguyIglesias <tanguyiglesias@gmail.com>
2022-06-16 16:31:20 +0200

    debut fonctionnement js
    après avoir regarder quelque video, j'ai réussi à faire fonctionner l'API

--- script.js
+++ script.js
@@ -1,59 +1,24 @@
-const inputKeyUp = (e) => {
-    if (e.keyCode === 13) {
-        appUp();
-    }
-}
+
+
 document.querySelector('#input-town').addEventListener('keyup', (e) => {
-    inputKeyUp(e);
+    if (e.keyCode === 13) {
+        let city = e.target.value;
+        let key = 'adc743d149ca38cafb6f5022f11f69dc';
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric&mode=json&lang=fr`;
+        console.log(this.value)
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                let affichage = '<ul>';
+                 
+                    affichage += `<li>${data.name} : il fait ${Math.floor(Math.ceil(data.main.temp))} °C ${data.weather[0].main}</li>`;
+                
+                affichage += '</ul>';
+                document.querySelector('#meteo-jour').innerHTML = affichage;
+            })
+    }
 });
 
 
-async function appUp() {
-    try {
 
-        let city = document.getElementById('input-town').value;
-
-        let key = 'adc743d149ca38cafb6f5022f11f69dc';
-
-        let weather = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`);
-        let forecast = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${key}`);
-
-        let objects = weather.json();
-        let call = forecast.json();
-
-        let weatherPost = (element) => {
-            element.then((array) => {
-                try {
-                    erreur.style.visibility = 'hidden';
-                    output.style.visibility = 'visible';
-                    output.insertAdjacentHTML('afterbegin', `
-                    <div>
-                        <h2 class="output__h2">${city.charAt(0).toUpperCase() + city.slice(1).toLowerCase()}</h2>},${array.sys.country}</h2>
-                        <img src="http://openweathermap.org/img/wn/${arrays.weather[0].icon}.png">
-                        <h3 class="output__h3"> Humidity: ${array.main.humidity}%</h3>
-                        <h3 class="output__h3"> Weather: ${array.weather[0].main}</h3>
-                        <h3 class="output__h3"> Temperature: ${Math.floor(Math.ceil(array.main.temp))} °C</h3>
-                        <h3 class="output__h3"> Min : ${Math.floor(Math.ceil(array.main.temp_min))} °C Max: ${Math.floor(Math.ceil(arrays.main.temp_max))} °C</h3>
-                    </div>
-                        `)
-                }
-                catch (err) {
-                    document.querySelector('.output').style.visibility = 'hidden';
-                    erreur.visibility = 'visible';
-                    erreur.style.color = 'red';
-                    erreur.innerText = 'City not found';
-                }
-            })
-        }
-
-
-        weatherPost(objects);
-    }
-    catch(err){ //error
-        forecastSec.style.visibility = 'hidden';
-        document.querySelector('.output').style.visibility = 'hidden';
-        erreur.style.visibility = 'visible';
-        erreur.style.color = 'red'
-        erreur.innerText = "I can't communicate with the API";
-        console.log(err)
-}}
